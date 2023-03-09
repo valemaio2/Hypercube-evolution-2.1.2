6c5632ff99b19f6043c38cf60278a382da385480
valemaio2 <72505627+valemaio2@users.noreply.github.com>
2023-03-09 21:04:57 +0000

    fix G34

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -977,7 +977,7 @@
    * If not defined, probe limits will be used.
    * Override with 'M422 S<index> X<pos> Y<pos>'.
    */
-  #define Z_STEPPER_ALIGN_XY { {  10, 150 }, { 250,  150 } }
+  #define Z_STEPPER_ALIGN_XY { {  20, 150 }, { 250,  150 } }
 
   /**
    * Orientation for the automatically-calculated probe positions.
